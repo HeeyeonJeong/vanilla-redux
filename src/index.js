6b4b8b099b5bae634ac34ac63d19ceebef1e19a3
HeeyeonJeong <junghy14971@gmail.com>
2020-11-17 18:06:35 +0900

    Pure Redux,toDoList:state mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const paintToDo = (text) => {
-  const li = document.createElement("li");
-  ul.appendChild(li);
-  li.innerHTML = text;
+const ADD_TODO = "ADD_TODO";
+const DEL_TODO = "ADD_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action }];
+    case DEL_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const todo = input.value;
   input.value = "";
-  paintToDo(todo);
+  store.dispatch({ type: ADD_TODO, text: todo });
 };
 
 form.addEventListener("submit", handleSubmit);
